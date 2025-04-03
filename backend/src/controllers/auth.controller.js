cbab34b447d9fb8bb10a77f1e332fd8110096fcc
Kevin Rozario <rozariokevin73@gmail.com>
2025-04-03 11:11:08 +0530

    Enhance login functionality with improved validation, error handling, and token management

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,16 +1,50 @@
 import { ApiResponse } from "../utils/apiResponse.util.js";
+import { ApiError } from "../utils/apiError.util.js";
 import asyncHandler from "../utils/asyncHandler.util.js";
+import { User } from "../models/user.model.js";
 
-export const loginUser = asyncHandler((req, res) => {
+export const loginUser = asyncHandler(async (req, res) => {
+  // get data from request
+  const { email, userName, password } = req.body;
+
+  // validate data
+  if (!email || !userName || !password) {
+    throw new ApiError(400, { message: "All fields required!" });
+  }
+
+  // check if user exists
+  const user = await User.find({ email });
+  if (!user) {
+    throw new ApiError(400, { message: "User doesn't exists!" });
+  }
+
+  // check password correct
+  const isPasswordCorrect = user.isPasswordCorrect(password);
+  if (!isPasswordCorrect) {
+    throw new ApiError(400, { message: "Invalid password!" });
+  }
+
+  // generate access and refresh tokens
+  const accessToken = user.generateAccessToken();
+  const refreshToken = user.generateRefreshToken();
+
+  // save refresh token
+  user.refreshToken = refreshToken;
+  await user.save();
+
+  // set tokens as cookies
+  const cookieOptions = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production" ? true : false,
+    sameSite: "Strict",
+  };
+
+  // send success response
   res
-    .status(200)
-    .json(
-      new ApiResponse(
-        200,
-        { message: "User logged in successfully!" },
-        req.body,
-      ),
-    );
+    .cookie("accessToken", accessToken, cookieOptions)
+    .cookie("refreshToken", refreshToken, cookieOptions)
+    .status(201)
+    .json(new ApiResponse(200, { message: "User logged in successfully!" }));
 });
 
 export const registerUser = asyncHandler((req, res) => {
